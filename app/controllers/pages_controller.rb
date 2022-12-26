8e4def0872210b94c25e4d60022194489a04fa08
Addisu <addisuhaile87@gmail.com>
2022-12-26 11:45:53 +0300

    Configure the home.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
-  def home; end
+  class PagesController < ApplicationController
+    def home
+      @application = Doorkeeper::Application.find_by(name: 'Web Client')
+
+      @application = {
+        name: @application.name,
+        client_id: @application.uid,
+        client_secret: @application.secret
+      }
+    end
+  end
 end
